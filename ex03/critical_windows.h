4e697dfb8b08c294b29b905eec74ce65d67714a7
Edoardo Raccanello <edraccan@c2r2p2.42firenze.it>
2025-06-10 17:50:39 +0200

    ex02

---
+++ ex03/critical_windows.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   critical_windows.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edraccan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/10 17:40:52 by edraccan          #+#    #+#             */
+/*   Updated: 2025/06/10 17:41:21 by edraccan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CRITICAL_WINDOWS_H
+# define CRITICAL_WINDOWS_H
+
+# include <stdio.h>
+
+int count_critical_windows(const int *readings, int size);
+
+#endif
