077c65b6dec7587a61da58ba2ce8eb02cdcd956e
Edoardo Raccanello <edraccan@c2r2p2.42firenze.it>
2025-06-10 16:52:35 +0200

    ex01

---
+++ ex01/first_last.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   first_last.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edraccan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/10 16:35:50 by edraccan          #+#    #+#             */
+/*   Updated: 2025/06/10 16:36:45 by edraccan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIRST_LAST_H
+# define FIRST_LAST_H
+
+# include <stdio.h>
+
+void first_last(int arr[], int size, int target, int *first, int *last);
+
+#endif
